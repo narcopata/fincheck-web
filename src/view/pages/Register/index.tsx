1db08cb49bca12ecac1c6d7d465343ee96bb1acf
Rodrigo Walace Freitas da Silva <rodrigo.walace.silva@gmail.com>
2023-12-16 18:58:32 -0300

    refactor: using inline styles

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -3,23 +3,22 @@ import { FunctionComponent } from "preact";
 import { Input } from "../../components/Input";
 
 import { Button } from "../../components/Button";
-import styles from "./styles.module.css";
 
 export const Register: FunctionComponent = () => {
   return (
     <>
-      <header className={styles.header}>
-        <h1 className={styles["header-title"]}>Crie sua conta</h1>
+      <header className="flex flex-col items-center gap-4">
+        <h1 className="text-center text-gray-900 text-2xl font-bold tracking-[-1px]">Crie sua conta</h1>
 
         <p className="space-x-2">
-          <span className={styles["header-text"]}>Já possui uma conta?</span>
-          <a className={styles["header-link"]} href="/access/login">
+          <span className="text-gray-700 tracking-[-0.5px]">Já possui uma conta?</span>
+          <a className="text-teal-900 font-medium tracking-[-0.5px]" href="/access/login">
             Fazer login
           </a>
         </p>
       </header>
 
-      <form className={styles.form} action="action">
+      <form className="mt-[59px] flex flex-col gap-4" action="action">
         <Input name="name" placeholder="Nome" />
         <Input name="email" type="email" placeholder="E-mail" />
         <Input name="password" type="password" placeholder="Senha" />
