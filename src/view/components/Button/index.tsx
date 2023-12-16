8a3c396cd22f9ce3cdc21103933bbf28899d40c8
Rodrigo Walace Freitas da Silva <rodrigo.walace.silva@gmail.com>
2023-12-16 18:59:30 -0300

    refactor: using inline styles along with cn

--- src/view/components/Button/index.tsx
+++ src/view/components/Button/index.tsx
@@ -1,11 +1,11 @@
 import { ComponentProps, FunctionComponent } from "preact";
 
-import s from "./styles.module.css";
+import { cn } from "../../../app/utils/cn";
 
 type ButtonProps = ComponentProps<"button">;
 
 export const Button: FunctionComponent<ButtonProps> = ({className, type = "button", ...props}) => {
   return (
-    <button {...props} className={`${s.button} ${className}`} type={type} />
+    <button {...props} className={cn("hover:bg-teal-800 disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed text-white px-6 h-12 rounded-2xl font-medium transition-all", className)} type={type} />
   );
 };
