e10190b1a7e7a33ec872b09f2bb83528efdfa6dd
Rodrigo Walace Freitas da Silva <rodrigo.walace.silva@gmail.com>
2023-12-18 18:01:31 -0300

    feat: implementing register page with form validation

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -3,27 +3,37 @@ import { FunctionComponent } from "preact";
 import { Input } from "../../components/Input";
 
 import { Button } from "../../components/Button";
+import { useRegisterController } from "./useRegisterController";
 
 export const Register: FunctionComponent = () => {
+  const { errors, handleSubmit, register } = useRegisterController();
+
   return (
     <>
       <header className="flex flex-col items-center gap-4">
-        <h1 className="text-center text-gray-900 text-2xl font-bold tracking-[-1px]">Crie sua conta</h1>
+        <h1 className="text-center text-gray-900 text-2xl font-bold tracking-[-1px]">
+          Crie sua conta
+        </h1>
 
         <p className="space-x-2">
-          <span className="text-gray-700 tracking-[-0.5px]">Já possui uma conta?</span>
-          <a className="text-teal-900 font-medium tracking-[-0.5px]" href="/access/login">
+          <span className="text-gray-700 tracking-[-0.5px]">
+            Já possui uma conta?
+          </span>
+          <a
+            className="text-teal-900 font-medium tracking-[-0.5px]"
+            href="/access/login"
+          >
             Fazer login
           </a>
         </p>
       </header>
 
-      <form className="mt-[59px] flex flex-col gap-4" action="action">
-        <Input name="name" placeholder="Nome" />
-        <Input name="email" type="email" placeholder="E-mail" />
-        <Input name="password" type="password" placeholder="Senha" />
+      <form className="mt-[59px] flex flex-col gap-4" action="action" onSubmit={handleSubmit}>
+        <Input placeholder="Nome" {...register("name")} errorMessage={errors?.name?.message} />
+        <Input type="email" placeholder="E-mail" {...register("email")} errorMessage={errors?.email?.message} />
+        <Input type="password" placeholder="Senha" {...register("password")} errorMessage={errors?.password?.message} />
 
-        <Button type="button" className="mt-2 bg-teal-900">
+        <Button type="submit" className="mt-2 bg-teal-900">
           Criar conta
         </Button>
       </form>
