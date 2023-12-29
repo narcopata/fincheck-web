5ca2c1911c2f8111bffe134267852c761d45bc86
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-12-29 15:22:51 -0300

    refactor: wrapping whole application with tanstack query provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,23 @@
 import { render } from "preact";
 import { AppRouter } from "./Router";
 import "./style.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
 
 export function App() {
-  return <AppRouter />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />;
+    </QueryClientProvider>
+  );
 }
 
 render(<App />, document.getElementById("app"));
