9aaf9848216399e5db65affb6b299c73ba5e3482
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-01-02 21:30:35 -0300

    feat: creating mocked user profile menu component

---
+++ src/view/components/UserMenu.tsx
@@ -0,0 +1,9 @@
+export const UserMenu = () => {
+  return (
+    <div className="bg-teal-0 rounded-full w-full h-full flex items-center justify-center">
+      <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium border border-teal-100">
+        RS
+      </span>
+    </div>
+  );
+};
