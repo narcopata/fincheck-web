694d75899d84dbb35240550532291c0beb2d93b7
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-01-02 21:32:17 -0300

    feat: creating Dashboard (partial) component

---
+++ src/view/pages/Dashboard/index.tsx
@@ -0,0 +1,16 @@
+import { Logo } from "../../components/Logo";
+import { UserMenu } from "../../components/UserMenu";
+
+export const Dashboard = () => {
+  return (
+    <div className="bg-red-500 h-full w-full px-8 pb-8 pt-6 flex flex-col gap-4">
+      <header className="h-12 items-center justify-between">
+        <Logo className="h-6 text-teal-900" />
+        <UserMenu />
+      </header>
+      <main className="flex-1">
+        Content
+      </main>
+    </div>
+  );
+};
