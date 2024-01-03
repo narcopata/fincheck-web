8e408ec7b1abce8dd3101676c6dbd5e9a04a3a7d
Rodrigo Walace Freitas da Silva <52475846+narcopata@users.noreply.github.com>
2024-01-03 16:32:43 -0300

    refactor: using transactions and accounts components on Dashboard

--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -1,15 +1,22 @@
 import { Logo } from "../../components/Logo";
 import { UserMenu } from "../../components/UserMenu";
+import { Accounts } from "./components/Accounts";
+import { Transactions } from "./components/Transactions";
 
 export const Dashboard = () => {
   return (
-    <div className="bg-red-500 h-full w-full px-8 pb-8 pt-6 flex flex-col gap-4">
+    <div className="bg-red-500 h-full p-4 md:px-8 md:pb-8 md:pt-6 w-full px-8 pb-8 pt-6 flex flex-col gap-4">
       <header className="h-12 items-center justify-between">
         <Logo className="h-6 text-teal-900" />
         <UserMenu />
       </header>
-      <main className="flex-1">
-        Content
+      <main className="flex-1 flex flex-col md:flex-row gap-4">
+        <div className="w-full md:w-1/2">
+          <Accounts />
+        </div>
+        <div className="w-full md:w-1/2">
+          <Transactions />
+        </div>
       </main>
     </div>
   );
