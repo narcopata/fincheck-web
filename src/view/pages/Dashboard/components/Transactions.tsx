e25ba78cd38e64feb0e8c473b55542f478918f3b
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-01-03 22:57:17 -0300

    fix: fixing style

--- src/view/pages/Dashboard/components/Transactions.tsx
+++ src/view/pages/Dashboard/components/Transactions.tsx
@@ -1,6 +1,6 @@
 export const Transactions = () => {
   return (
-    <div className="bg-teal-900 rounded-2xl w-full h-full p-10">
+    <div className="bg-gray-100 rounded-2xl w-full h-full p-10">
       Transactions
     </div>
   );
