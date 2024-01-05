4b05d37eee890dacfb85c28a9302d89e9cae4b4c
Rodrigo Walace Freitas da Silva <52475846+narcopata@users.noreply.github.com>
2024-01-05 18:20:00 -0300

    fix: fixing right button style

--- src/view/pages/Dashboard/components/Accounts.tsx
+++ src/view/pages/Dashboard/components/Accounts.tsx
@@ -36,7 +36,7 @@ export const Accounts = () => {
             </button>
             <button
               type="button"
-              className="py-3 pl-2.5 pr-3.5 rounded-full enabled:hover:bg-black/10 transition-colors disabled:opacity-40"
+              className="py-3 pl-3.5 pr-2.5 rounded-full enabled:hover:bg-black/10 transition-colors disabled:opacity-40"
             >
               <ChevronRightIcon className="text-white w-6 h-6" />
             </button>
