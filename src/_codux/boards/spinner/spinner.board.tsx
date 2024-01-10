2339e7385d08eefebeffce48b83651bb7694c835
Rodrigo Walace Freitas da Silva <52475846+narcopata@users.noreply.github.com>
2024-01-10 18:57:56 -0300

    chore: deleting useless file

--- src/_codux/boards/spinner/spinner.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from "@wixc3/react-board";
-import { Spinner } from "../../../view/components/Spinner";
-
-export default createBoard({
-  name: "Spinner",
-  Board: () => <Spinner />,
-  isSnippet: true,
-});
