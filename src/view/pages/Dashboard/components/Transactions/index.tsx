462ebc4408592b82e164cf07af935653ac5611d0
Rodrigo Walace Freitas da Silva <52475846+narcopata@users.noreply.github.com>
2024-01-15 17:21:15 -0300

    refactor: including one more transaction

--- src/view/pages/Dashboard/components/Transactions/index.tsx
+++ src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -2,19 +2,23 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { MONTHS } from "../../../../../app/constants/months";
 import { ChevronDownIcon } from "../../../../../types/radix-icons";
 
+import { formatCurrency } from "../../../../../app/utils/formatCurrency";
 import { FilterIcon } from "../icons/FilterIcon";
 import { TransactionsIcon } from "../icons/TransactionsIcon";
+import { CategoryIcon } from "../icons/categories/CategoryIcon";
 import { SliderNavigation } from "./SliderNavigation";
 import { SliderOption } from "./SliderOption";
 
 export const Transactions = () => {
   return (
-    <div className="bg-gray-100 rounded-2xl w-full h-full p-10">
+    <div className="bg-gray-100 rounded-2xl w-full h-full p-10 flex flex-col">
       <header>
         <div>
           <button className="flex items-center gap-2" type="button">
             <TransactionsIcon />
-            <span className="text-sm text-gray-800 tracking-[-0.5px] font-medium">Transações</span>
+            <span className="text-sm text-gray-800 tracking-[-0.5px] font-medium">
+              Transações
+            </span>
             <ChevronDownIcon className="text-gray-900" />
           </button>
 
@@ -42,7 +46,41 @@ export const Transactions = () => {
         </div>
       </header>
 
-      <main className="mt-4" />
+      <main className="mt-4 space-y-2 max-h-full flex-1 overflow-y-auto h-64">
+        <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+          <div className="flex-1 flex items-center">
+            <CategoryIcon type="expense" />
+
+            <div>
+              <strong className="font-bold tracking-[-0.5px] block">
+                Almoço
+              </strong>
+              <span className="text-sm text-gray-600">04/06/2023</span>
+            </div>
+          </div>
+
+          <span className="tracking-[-0.5px] font-medium">
+            - {formatCurrency(123)}
+          </span>
+        </div>
+
+        <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+          <div className="flex-1 flex items-center">
+            <CategoryIcon type="income" />
+
+            <div>
+              <strong className="font-bold tracking-[-0.5px] block">
+                Almoço
+              </strong>
+              <span className="text-sm text-gray-600">04/06/2023</span>
+            </div>
+          </div>
+
+          <span className="tracking-[-0.5px] font-medium">
+            {formatCurrency(123)}
+          </span>
+        </div>
+      </main>
     </div>
   );
 };
