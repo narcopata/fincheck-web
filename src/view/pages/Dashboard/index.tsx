fc7d379d96c14ca65311a78a73898f4f8b95ed5d
Rodrigo Walace Freitas da Silva <52475846+narcopata@users.noreply.github.com>
2024-01-19 12:46:42 -0300

    refactor: wrapping up dashboard with its context

--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -2,22 +2,25 @@ import { Logo } from "../../components/Logo";
 import { UserMenu } from "../../components/UserMenu";
 import { Accounts } from "./components/Accounts";
 import { Transactions } from "./components/Transactions";
+import { DashboardProvider } from "./contexts/Dashboard";
 
 export const Dashboard = () => {
   return (
-    <div className="h-full w-full p-4 md:px-8 md:pb-8 md:pt-6 flex flex-col gap-4">
-      <header className="h-12 flex items-center justify-between">
-        <Logo className="h-6 text-teal-900" />
-        <UserMenu />
-      </header>
-      <main className="flex-1 flex flex-col md:flex-row gap-4">
-        <div className="w-full md:w-1/2">
-          <Accounts />
-        </div>
-        <div className="w-full md:w-1/2">
-          <Transactions />
-        </div>
-      </main>
-    </div>
+    <DashboardProvider value={null}>
+      <div className="h-full w-full p-4 md:px-8 md:pb-8 md:pt-6 flex flex-col gap-4">
+        <header className="h-12 flex items-center justify-between">
+          <Logo className="h-6 text-teal-900" />
+          <UserMenu />
+        </header>
+        <main className="flex-1 flex flex-col md:flex-row gap-4">
+          <div className="w-full md:w-1/2">
+            <Accounts />
+          </div>
+          <div className="w-full md:w-1/2">
+            <Transactions />
+          </div>
+        </main>
+      </div>
+    </DashboardProvider>
   );
 };
