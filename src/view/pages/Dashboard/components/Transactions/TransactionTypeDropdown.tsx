7994d5743180c9c14a058d693ecabf26183210c3
Rodrigo Walace Freitas da Silva <52475846+narcopata@users.noreply.github.com>
2024-01-25 19:12:05 -0300

    feat: creating dropdown menu for transaction types

---
+++ src/view/pages/Dashboard/components/Transactions/TransactionTypeDropdown.tsx
@@ -0,0 +1,40 @@
+import { ChevronDownIcon } from "@assets/icons/radix-icons";
+import { DropDownMenu } from "@components/DropdownMenu";
+import type { FunctionComponent } from "preact";
+import { ExpensesIcon } from "../icons/ExpensesIcon";
+import { IncomeIcon } from "../icons/IncomeIcon";
+import { TransactionsIcon } from "../icons/TransactionsIcon";
+
+// type Props = {};
+
+export const TransactionTypeDropdown: FunctionComponent = () => {
+  return (
+    <DropDownMenu.Root>
+      <DropDownMenu.Trigger>
+        <button className="flex items-center gap-2" type="button">
+          <TransactionsIcon />
+          <span className="text-sm text-gray-800 tracking-[-0.5px] font-medium">
+            Transações
+          </span>
+          <ChevronDownIcon className="text-gray-900" />
+        </button>
+      </DropDownMenu.Trigger>
+
+      <DropDownMenu.Portal>
+        <DropDownMenu.Content className="z-50 w-[280px]">
+          <DropDownMenu.Item className="gap-2">
+            <IncomeIcon />
+            Receitas
+          </DropDownMenu.Item>
+          <DropDownMenu.Item className="gap-2">
+            <ExpensesIcon /> Despesas
+          </DropDownMenu.Item>
+          <DropDownMenu.Item className="gap-2">
+            <TransactionsIcon /> Transações
+          </DropDownMenu.Item>
+        </DropDownMenu.Content>
+      </DropDownMenu.Portal>
+    </DropDownMenu.Root>
+  );
+};
+/*  */
