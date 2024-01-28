f65d6cf2c254ac6c5f3906405dd9eeae8577a9ca
Rodrigo Walace Freitas da Silva <52475846+narcopata@users.noreply.github.com>
2024-01-28 14:29:22 -0300

    refactor: creating resources for handling modal visibility

--- src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
+++ src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
@@ -1,12 +1,25 @@
+import { useCallback, useState } from "preact/hooks";
 import { useDashboard } from "../../contexts/Dashboard/useDashboard";
 
 export const useTransactionsController = () => {
   const { areValuesVisible } = useDashboard();
+  const [isFiltersModalOpen, setIsFiltersModalOpen] = useState(false);
+
+  const handleOpenFiltersModal = useCallback(() => {
+    setIsFiltersModalOpen(true);
+  }, []);
+
+  const handleCloseFiltersModal = useCallback(() => {
+    setIsFiltersModalOpen(false);
+  }, []);
 
   return {
     areValuesVisible,
     isFirstLoading: true,
     isNextLoading: false,
     transactions: [],
+    isFiltersModalOpen,
+    handleOpenFiltersModal,
+    handleCloseFiltersModal,
   };
 };
