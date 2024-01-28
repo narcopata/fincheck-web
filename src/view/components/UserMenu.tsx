2f0da7b254e4214861ef724ed58f9024512d1757
Rodrigo Walace Freitas da Silva <52475846+narcopata@users.noreply.github.com>
2024-01-28 14:25:01 -0300

    refactor: finishing user menu dropdown component

--- src/view/components/UserMenu.tsx
+++ src/view/components/UserMenu.tsx
@@ -1,9 +1,31 @@
+import { ExitIcon } from "@assets/icons/radix-icons";
+import { useAuthContext } from "../../app/contexts/AuthContext";
+import { DropDownMenu } from "./DropdownMenu";
+
 export const UserMenu = () => {
+  const { signout } = useAuthContext();
+
   return (
-    <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
-      <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
-        RS
-      </span>
-    </div>
+    <DropDownMenu.Root>
+      <DropDownMenu.Trigger>
+        <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
+          <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
+            RS
+          </span>
+        </div>
+      </DropDownMenu.Trigger>
+
+      <DropDownMenu.Portal>
+        <DropDownMenu.Content className="w-32">
+          <DropDownMenu.Item
+            onselect={signout}
+            className="flex items-center justify-between"
+          >
+            Sair
+            <ExitIcon className="w-4 h-4" />
+          </DropDownMenu.Item>
+        </DropDownMenu.Content>
+      </DropDownMenu.Portal>
+    </DropDownMenu.Root>
   );
 };
