3b21767d204d871e1257b35d4f28776cb6c68931
Rodrigo Walace Freitas da Silva <52475846+narcopata@users.noreply.github.com>
2024-01-28 14:25:37 -0300

    feat: creating dashboard fab button component

---
+++ src/view/pages/Dashboard/components/Fab/index.tsx
@@ -0,0 +1,37 @@
+import { PlusIcon } from "@assets/icons/radix-icons";
+import { DropDownMenu } from "@components/DropdownMenu";
+import type { FunctionComponent } from "preact";
+import { BankAccountIcon } from "../icons/BankAccountIcon";
+import { CategoryIcon } from "../icons/categories/CategoryIcon";
+
+export const Fab: FunctionComponent = () => {
+  return (
+    <div className="fixed right-4 bottom-4">
+      <DropDownMenu.Root>
+        <DropDownMenu.Trigger>
+          <button
+            className=" text-white bg-teal-900 rounded-full flex items-center justify-center"
+            type="button"
+          >
+            <PlusIcon className="w-6 h-6" />
+          </button>
+        </DropDownMenu.Trigger>
+
+        <DropDownMenu.Content>
+          <DropDownMenu.Item className="gap-2">
+            <CategoryIcon type="expense" />
+            Nova Despesa
+          </DropDownMenu.Item>
+          <DropDownMenu.Item className="gap-2">
+            <CategoryIcon type="income" />
+            Nova Receita
+          </DropDownMenu.Item>
+          <DropDownMenu.Item className="gap-2">
+            <BankAccountIcon />
+            Nova Conta
+          </DropDownMenu.Item>
+        </DropDownMenu.Content>
+      </DropDownMenu.Root>
+    </div>
+  );
+};
