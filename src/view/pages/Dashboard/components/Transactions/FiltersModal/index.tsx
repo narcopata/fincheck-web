66e93cb686998f6b24047c7f7d09694aa065a9ea
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-01-28 21:04:42 -0300

    refactor: fixing styles and changing element structure

--- src/view/pages/Dashboard/components/Transactions/FiltersModal/index.tsx
+++ src/view/pages/Dashboard/components/Transactions/FiltersModal/index.tsx
@@ -21,73 +21,75 @@ export const FiltersModal: FunctionComponent<Props> = ({ open, onClose }) => {
   return (
     <Modal.Root open={open}>
       <Modal.Header onClose={onClose} title="Filtros" />
-      <Modal.Content>
-        <span className="text-lg tracking-[-1px] font-bold text-gray-800">
-          Conta
-        </span>
+        <div>
+          <span className="text-lg tracking-[-1px] font-bold text-gray-800">
+            Conta
+          </span>
 
-        <div className="space-y-2 mt-2">
-          {Array.from(accounts).map((value) => (
-            <button
-              key={value.id}
-              type="button"
-              onClick={() =>
-                dispatchFilters({ set: { bankAccountId: value.id } })
-              }
-              className={cn(
-                "p-2 rounded-2xl w-full text-left hover:bg-gray-50 transition-colors",
-                value.id === filtersData.bankAccountId && "!bg-gray-200",
-              )}
-            >
-              {value.title}
-            </button>
-          ))}
+          <div className="space-y-2 mt-2">
+            {Array.from(accounts).map((value) => (
+              <button
+                key={value.id}
+                type="button"
+                onClick={() =>
+                  dispatchFilters({ set: { bankAccountId: value.id } })
+                }
+                className={cn(
+                  "p-2 rounded-2xl w-full text-left text-gray-800 hover:bg-gray-50 transition-colors",
+                  value.id === ("100" || filtersData.bankAccountId) &&
+                    "!bg-gray-200",
+                )}
+              >
+                {value.title}
+              </button>
+            ))}
+          </div>
         </div>
 
-        <div className="mt-10">
+        <div className="mt-10 text-gray-800">
           <span className="text-lg tracking-[-1px] font-bold text-gray-800">
             Ano
           </span>
-        </div>
 
-        <div className="mt-2 w-52 flex items-center justify-between">
-          <button
-            type="button"
-            className="w-12 h-12 flex items-center justify-center"
-            onClick={() =>
-              dispatchFilters({
-                set: {
-                  year: {
-                    type: "decrement",
+          <div className="mt-2 w-52 flex items-center justify-between">
+            <button
+              type="button"
+              className="w-12 h-12 flex items-center justify-center"
+              onClick={() =>
+                dispatchFilters({
+                  set: {
+                    year: {
+                      type: "decrement",
+                    },
                   },
-                },
-              })
-            }
-          >
-            <ChevronLeftIcon className="w-6 h-6" />
-          </button>
+                })
+              }
+            >
+              <ChevronLeftIcon className="w-6 h-6" />
+            </button>
 
-          <div className="flex-1 text-center">
-            <span className="text-sm font-medium tracking-[-0.5px]">
-              {filtersData.year}
-            </span>
+            <div className="flex-1 text-center">
+              <span className="text-sm font-medium tracking-[-0.5px]">
+                {filtersData.year}
+              </span>
+            </div>
+
+            <button
+              type="button"
+              className="w-12 h-12 flex items-center justify-center"
+              onClick={() =>
+                dispatchFilters({
+                  set: {
+                    year: {
+                      type: "increment",
+                    },
+                  },
+                })
+              }
+            >
+              <ChevronRightIcon className="w-6 h-6" />
+            </button>
           </div>
-
-          <button
-            type="button"
-            className="w-12 h-12 flex items-center justify-center"
-            onClick={() =>
-              dispatchFilters({
-                set: {
-                  year: {
-                    type: "increment",
-                  },
-                },
-              })
-            }
-          >
-            <ChevronRightIcon className="w-6 h-6" />
-          </button>
         </div>
 
         <Button className="w-full mt-10">
