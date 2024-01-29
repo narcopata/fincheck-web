f5e1efe675673cc1bd49b1a614a0d0be60cbc6c7
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-01-29 12:26:14 -0300

    refactor: using new modal content and applying missing style

--- src/view/pages/Dashboard/components/Transactions/TransactionTypeDropdown.tsx
+++ src/view/pages/Dashboard/components/Transactions/TransactionTypeDropdown.tsx
@@ -1,5 +1,6 @@
-import { ChevronDownIcon } from "@assets/icons/radix-icons";
 import { DropDownMenu } from "@components/DropdownMenu";
+
+import { ChevronDownIcon } from "@assets/icons/radix-icons";
 import type { FunctionComponent } from "preact";
 import { ExpensesIcon } from "../icons/ExpensesIcon";
 import { IncomeIcon } from "../icons/IncomeIcon";
@@ -18,20 +19,18 @@ export const TransactionTypeDropdown: FunctionComponent = () => {
         </button>
       </DropDownMenu.Trigger>
 
-      <DropDownMenu.Portal>
-        <DropDownMenu.Content className="z-50 w-[280px]">
-          <DropDownMenu.Item className="gap-2">
-            <IncomeIcon />
-            Receitas
-          </DropDownMenu.Item>
-          <DropDownMenu.Item className="gap-2">
-            <ExpensesIcon /> Despesas
-          </DropDownMenu.Item>
-          <DropDownMenu.Item className="gap-2">
-            <TransactionsIcon /> Transações
-          </DropDownMenu.Item>
-        </DropDownMenu.Content>
-      </DropDownMenu.Portal>
+      <DropDownMenu.Content className="w-[280px]">
+        <DropDownMenu.Item className="gap-2">
+          <IncomeIcon />
+          Receitas
+        </DropDownMenu.Item>
+        <DropDownMenu.Item className="gap-2">
+          <ExpensesIcon /> Despesas
+        </DropDownMenu.Item>
+        <DropDownMenu.Item className="gap-2">
+          <TransactionsIcon /> Transações
+        </DropDownMenu.Item>
+      </DropDownMenu.Content>
     </DropDownMenu.Root>
   );
 };
