20c311576286317181178d3044bf3bc5e7aa35f7
Rodrigo Walace Freitas da Silva <52475846+narcopata@users.noreply.github.com>
2024-02-03 01:19:03 -0300

    feat: creating color dropdown menu input component

---
+++ src/view/components/ColorDropdownInput.tsx
@@ -0,0 +1,76 @@
+import { ChevronDownIcon } from "@assets/icons/radix-icons";
+import { COLORS, type Color } from "@constants/colors";
+import { CrossCircledIcon } from "@radix-ui/react-icons";
+import { cn } from "@utils/cn";
+import type { FunctionComponent } from "preact";
+import { useState } from "preact/hooks";
+import { ColorIcon } from "../pages/Dashboard/components/icons/ColorIcon";
+import { DropDownMenu } from "./DropdownMenu";
+
+type Props = Partial<HTMLSelectElement> & {
+  errorMessage?: string;
+};
+
+export const ColorDropdownInput: FunctionComponent<Props> = ({
+  errorMessage,
+  className,
+}) => {
+  const [selectedColor, setSelectedColor] = useState<Color | null>(null);
+
+  const handleSelect = (color: Color) => {
+    setSelectedColor(color);
+  }
+
+  return (
+    <form>
+      <DropDownMenu.Root>
+        <DropDownMenu.Trigger>
+          <button
+            className={cn(
+              "bg-white rounded-lg border border-gray-500 focus:border-gray-800 text-gray-700 w-full h-[52px] px-3 outline-none transition-all text-left relative",
+              {
+                "!border-red-900": !!errorMessage,
+              },
+              className,
+            )}
+            type="button"
+          >
+            Cor
+
+            <div className="absolute right-3 top-1/2 -translate-y-1/2">
+              {!selectedColor && <ChevronDownIcon
+                className="w-6 h-6 text-gray-800"
+              />}
+              {
+                selectedColor && <ColorIcon
+                  {...selectedColor}
+                />
+              }
+            </div>
+          </button>
+        </DropDownMenu.Trigger>
+
+        <DropDownMenu.Content
+          className="grid grid-cols-4"
+        >
+          {COLORS.map((color) => (
+            <DropDownMenu.Item
+            onselect={
+              () => handleSelect(color)
+            }
+            key={color.bg}>
+              <ColorIcon {...color} />
+            </DropDownMenu.Item>
+          ))}
+        </DropDownMenu.Content>
+      </DropDownMenu.Root>
+
+      {errorMessage && (
+        <div className="flex gap-2 items-center mt-2 text-red-900">
+          <CrossCircledIcon />
+          <span className="text-xs">{errorMessage}</span>
+        </div>
+      )}
+    </form>
+  );
+};
