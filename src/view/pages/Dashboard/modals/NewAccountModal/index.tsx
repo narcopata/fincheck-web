7ef7fdaca4b2113e240a19b33e7b0874220f4e72
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-02-09 20:11:24 -0300

    refactor: registering inputs and adding form error messages

--- src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
+++ src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
@@ -8,25 +8,35 @@ import type { FunctionComponent } from "preact";
 import { useNewAccountModal } from "./useNewAccountModal";
 
 export const NewAccountModal: FunctionComponent = () => {
-  const { modals } = useNewAccountModal();
+  const { modals, form } = useNewAccountModal();
 
   return (
     <Modal.Root open={modals.newAccount.isOpen}>
       <Modal.Header title="Nova Conta" onClose={modals.newAccount.close} />
 
-      <form>
+      <form onSubmit={form.handleSubmit}>
         <div>
           <span className="text-gray-600 tracking-[-0.5px] text-xs">Saldo</span>
           <div className="flex items-center gap-2">
             <span className="text-gray-600 tracking-[-0.5px] text-lg">R$</span>
-            <InputCurrency />
+            <InputCurrency
+              errorMessage={form.errors.initialBalance?.message}
+              {...form.register("initialBalance")}
+            />
           </div>
         </div>
         <div className="mt-10 flex flex-col gap-4">
-          <Input type="text" name="name" placeholder="Nome da conta" />
+          <Input
+            type="text"
+            name="name"
+            placeholder="Nome da conta"
+            errorMessage={form.errors.name?.message}
+            {...form.register("name")}
+          />
 
           <Select
             placeholder="Tipo"
+            errorMessage={form.errors.type.message}
             options={[
               {
                 value: "CHECKING",
@@ -41,9 +51,13 @@ export const NewAccountModal: FunctionComponent = () => {
                 label: "Dinheiro Físico",
               },
             ]}
+            {...form.register("type")}
           />
 
-          <ColorDropdownInput />
+          <ColorDropdownInput
+            errorMessage={form.errors.color?.message}
+            {...form.register("color")}
+          />
 
           <Button>Salvar</Button>
         </div>
