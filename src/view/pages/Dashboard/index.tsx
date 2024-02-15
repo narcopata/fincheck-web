3618ed40e82b0c46ffeda9e0fbd8f1058bfc4313
Rodrigo Walace Freitas da Silva <52475846+narcopata@users.noreply.github.com>
2024-02-14 22:47:17 -0300

    refactor: enabling modal

--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -3,32 +3,38 @@ import { UserMenu } from "@components/UserMenu";
 import { Accounts } from "./components/Accounts";
 import { Fab } from "./components/Fab";
 import { Transactions } from "./components/Transactions";
-import { DashboardProvider } from "./contexts/Dashboard";
+import { DashboardContext, DashboardProvider } from "./contexts/Dashboard";
+import { EditAccountModal } from "./modals/EditAccountModal";
 import { NewAccountModal } from "./modals/NewAccountModal";
 import { NewTransactionModal } from "./modals/NewTransactionModal";
 
 export const Dashboard = () => {
   return (
     <DashboardProvider value={null}>
-      <div className="h-full w-full p-4 md:px-8 md:pb-8 md:pt-6 flex flex-col gap-4">
-        <header className="h-12 flex items-center justify-between">
-          <Logo className="h-6 text-teal-900" />
-          <UserMenu />
-        </header>
-        <main className="flex-1 flex flex-col md:flex-row gap-4">
-          <div className="w-full md:w-1/2">
-            <Accounts />
-          </div>
-          <div className="w-full md:w-1/2">
-            <Transactions />
-          </div>
-        </main>
+      <DashboardContext.Consumer>
+        {(ctx) => (
+          <div className="h-full w-full p-4 md:px-8 md:pb-8 md:pt-6 flex flex-col gap-4">
+            <header className="h-12 flex items-center justify-between">
+              <Logo className="h-6 text-teal-900" />
+              <UserMenu />
+            </header>
+            <main className="flex-1 flex flex-col md:flex-row gap-4">
+              <div className="w-full md:w-1/2">
+                <Accounts />
+              </div>
+              <div className="w-full md:w-1/2">
+                <Transactions />
+              </div>
+            </main>
 
-        <Fab />
+            <Fab />
 
-        <NewAccountModal />
-        <NewTransactionModal />
-      </div>
+            <NewAccountModal />
+            <NewTransactionModal />
+            {ctx?.modals.editAccount.account && <EditAccountModal />}
+          </div>
+        )}
+      </DashboardContext.Consumer>
     </DashboardProvider>
   );
 };
