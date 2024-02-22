1960c8aefa9a5c4fd33abda1153aa7b8b0daa2f9
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-02-21 23:05:04 -0300

    style: formatting

--- src/app/entities/Category.ts
+++ src/app/entities/Category.ts
@@ -1,6 +1,6 @@
 export type Category = {
-    id: string;
-    name: string;
-    icon: string;
-    type: "income" | "expense";
-}
+  id: string;
+  name: string;
+  icon: string;
+  type: "income" | "expense";
+};
