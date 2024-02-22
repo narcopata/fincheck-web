c7c2dbe3a394c35fc26b9bd0235c476856b5dad0
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-02-21 23:02:55 -0300

    feat: creating service for fetching all categories

--- src/app/services/categories/getAll.ts
+++ src/app/services/categories/getAll.ts
@@ -1,8 +1,8 @@
 import { httpClient } from "@services/httpClient";
-import { Category } from "../../entities/Category";
+import type { Category } from "../../entities/Category";
 
 export const getAll = async (): Promise<Category[]> => {
-    const categories = await httpClient.get("categories/mine").json<Category[]>();
+  const categories = await httpClient.get("categories/mine").json<Category[]>();
 
-    return categories;
-}
+  return categories;
+};
