70eca5a08502ba6d8f4dd232ba0a7ee90be59e02
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-02-21 23:03:57 -0300

    feat: creating query key for previous service

--- src/app/config/queryKeys.ts
+++ src/app/config/queryKeys.ts
@@ -2,4 +2,5 @@ export const QUERY_KEYS = Object.freeze({
   USERS_ME: Object.freeze(["users", "me"] as const),
   BANK_ACCOUNTS_ALL: Object.freeze(["bankAccounts", "all"] as const),
   BANK_ACCOUNTS_EDIT: Object.freeze(["bankAccounts", "edit"] as const),
+  CATEGORIES_GET_ALL: Object.freeze(["categories", "all"] as const),
 });
