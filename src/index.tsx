496aab8da5066795320e44f4444d95a1bacb18ca
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-02-29 20:45:50 -0300

    fix: enabling toast

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { AuthContextProvider } from "@contexts/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { render } from "preact";
+import { Toaster } from "react-hot-toast";
+
 import { AppRouter } from "./Router";
 
 import "./style.css";
@@ -19,6 +21,7 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider value={null}>
+        <Toaster />
         <AppRouter />;
       </AuthContextProvider>
 
