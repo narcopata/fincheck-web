83460c5f0556221dd3f36f1c1b95a27735a5243c
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-02-29 20:32:59 -0300

    refactor: using type from entity

--- src/app/services/user/me.ts
+++ src/app/services/user/me.ts
@@ -1,9 +1,7 @@
+import type { User } from "@entities/User";
 import { httpClient } from "../httpClient";
 
-type Response = {
-  name: string;
-  email: string;
-};
+type Response = User;
 
 export const me = async (): Promise<Response> => {
   const data = await httpClient.get("users/me").json<Response>();
