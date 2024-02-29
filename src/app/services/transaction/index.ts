2c008b3a91a2b2b66e5ab735c251a43c0c1680f0
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-02-29 20:32:32 -0300

    feat: creating service for deleting transactions

--- src/app/services/transaction/index.ts
+++ src/app/services/transaction/index.ts
@@ -1,11 +1,13 @@
 import { create } from "./create";
 import { type GetAllTransactionsParams, getAll } from "./getAll";
+import { remove } from "./remove";
 import { update } from "./update";
 
 export const transactionService = {
   create,
   getAll,
   update,
+  remove,
 };
 
 export type { GetAllTransactionsParams };
